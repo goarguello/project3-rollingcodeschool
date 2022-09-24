94e4a08eaed21485250161e4f75fb6009d59e53b
Gonzalo Nicolás Argüello <gon01.dev@gmail.com>
2022-09-24 09:46:46 -0300

    Limpieza de proyecto

--- src/components/NavbarComponent/NavbarComponent.js
+++ src/components/NavbarComponent/NavbarComponent.js
@@ -7,7 +7,7 @@ const NavbarComponent = () => {
       <Container>
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#home">Login</Nav.Link>
           <Nav.Link href="#features">Features</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
