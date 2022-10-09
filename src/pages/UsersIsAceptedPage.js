122d93e61648dba5610739537bb693f05386df95
unknown <Dan1el-aguilar>
2022-10-09 19:47:24 -0300

    add/pageUserIsAcepted

---
+++ src/pages/UsersIsAceptedPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import UsersIsAcepted from '../components/UsersIsAcepted/UsersIsAcepted'
+
+const UsersIsAceptedPage = () => {
+  return (
+    <div className='container'>
+      <h1>Pagina de aceptar Usuarios</h1>
+      <UsersIsAcepted/>
+    </div>
+  )
+}
+
+export default UsersIsAceptedPage
