e6f46b4a5cbaf01b45ba611743c167576b11046f
Gonzalo Nicolás Argüello <gon01.dev@gmail.com>
2022-10-12 19:16:42 -0300

    Ultimos estilos

--- src/components/EditAlForm/EditAlForm.js
+++ src/components/EditAlForm/EditAlForm.js
@@ -2,69 +2,94 @@ import { useEffect, useState } from "react";
 import { Form, Button } from "react-bootstrap";
 import axiosConfig from "../../config/axiosConfig";
 
-const EditAlForm = ({getStudents, handleCloseEdit, selected}) => {
-    const [values, setValues] = useState({
-        nameCompleted:'',
-        curse:'',
-        phone:'',
-        address:''
+const EditAlForm = ({ getStudents, handleCloseEdit, selected }) => {
+  const [values, setValues] = useState({});
+  const [valuesModal, setValuesModal] = useState({});
+  const getStudent = async () => {
+    try {
+      const response = await axiosConfig.get("/alumns/" + selected);
+      setValuesModal(response.data.alumn);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
+
+  const handleChange = (e) => {
+    setValues({
+      ...values,
+      [e.target.name]: e.target.value,
     });
-    const handleChange = (e)=>{
-        setValues({
-            ...values,
-            [e.target.name] : e.target.value
-        })
-    }
-    const getStudent = async()=>{
-        try {
-            const response = await axiosConfig.get('/alumns/'+selected);
-            setValues(response.data)
-        } catch (error) {
-            alert(error.message)
-        }
-    }
+  };
+  useEffect(() => {
+    getStudent();
+  }, []);
 
-    useEffect(()=>{
-        getStudent()
-    },[]);
-
-    const handleSubmit = async(e) =>{
-        try {
-            e.preventDefault();
-            await axiosConfig.put('/alumns/'+selected,values)
-            getStudents()
-        } catch (error) {
-            alert('Error al editar un alumno')
-        }
+  const handleSubmit = async (e) => {
+    try {
+      e.preventDefault();
+      await axiosConfig.put("/alumns/" + selected, values);
+      getStudents();
+    } catch (error) {
+      alert("Error al editar un alumno");
     }
+  };
 
-    return ( 
-        <Form onSubmit={handleSubmit}>
-      <Form.Group className="mb-3" >
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group className="mb-3">
         <Form.Label>Nombre completo</Form.Label>
-        <Form.Control type="text" name="nameCompleted" onChange={handleChange} value={values.nameCompleted} />
+        <div>{valuesModal.nameCompleted}</div>
+        <Form.Control
+          type="text"
+          name="nameCompleted"
+          onChange={handleChange}
+          value={values.nameCompleted}
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
+      <Form.Group className="mb-3">
         <Form.Label>Curso</Form.Label>
-        <Form.Control type="number" name="curse" onChange={handleChange} value={values.curse}  />
+        <div>{valuesModal.curse}</div>
+        <Form.Control
+          type="text"
+          name="curse"
+          onChange={handleChange}
+          value={values.curse}
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
+      <Form.Group className="mb-3">
         <Form.Label>Telefono</Form.Label>
-        <Form.Control type="number" name="phone" onChange={handleChange} value={values.phone} />
+        <div>{valuesModal.phone}</div>
+        <Form.Control
+          type="number"
+          name="phone"
+          onChange={handleChange}
+          value={values.phone}
+        />
       </Form.Group>
 
-      <Form.Group className="mb-3" >
+      <Form.Group className="mb-3">
         <Form.Label>Direccion</Form.Label>
-        <Form.Control type="text" name="address" onChange={handleChange} value={values.address} />
+        <div>{valuesModal.adress}</div>
+        <Form.Control
+          type="text"
+          name="adress"
+          onChange={handleChange}
+          value={values.adress}
+        />
       </Form.Group>
-      
-      <Button className="btn-alumns" variant="primary" type="submit" onClick={handleCloseEdit} >
+
+      <Button
+        className="btn-alumns"
+        variant="primary"
+        type="submit"
+        onClick={handleCloseEdit}
+      >
         Editar
       </Button>
     </Form>
-     );
-}
- 
+  );
+};
+
 export default EditAlForm;
