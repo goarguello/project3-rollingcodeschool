e346b0fe0e74a5a2b1465e04c747a9a5737b902c
unknown <Dan1el-aguilar>
2022-10-18 12:09:39 -0300

    add/redirects

--- src/pages/AdminPage.js
+++ src/pages/AdminPage.js
@@ -8,7 +8,7 @@ const AdminPage = () => {
     <CardComponent img={"https://as2.ftcdn.net/v2/jpg/01/93/92/99/1000_F_193929975_aljh2eRP7pZ5MhnuO9R3WrbcEpQGkljB.jpg"} url={"/subject-admin"} name={"Materias"} />
     <CardComponent img={"https://media.istockphoto.com/photos/teenage-school-kids-smiling-to-camera-in-school-corridor-picture-id826212368?b=1&k=20&m=826212368&s=170667a&w=0&h=gYQ2zFTRSClCOoGI0P__MmDjFZZvQkKhcYC7_zDZefk="} url={"/students-admin"} name={"Alumnos"} />
     <CardComponent img={"https://mundohol.cordoba.gob.ar/wp-content/uploads/sites/5/elementor/thumbs/Manual-de-usuario-2-pey55ogoumj9xeul3rxw305q7npn1aomf3wppkx1o8.png"} url={"/isacepted"} name={"Habilitación"} />
-    {/* <Link to='/isacepted'><button>Habilitar o inhabilitar usuarios</button></Link> */}
+    
 </div>
     );
 }
